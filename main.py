09d24aa53dfd9f5239a1def9748ab6d0a7f5cca1
Hugo Ferreira <71095540+guhzoide@users.noreply.github.com>
2022-03-23 17:43:26 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg
+import subprocess
+
+def main():
+    dados = subprocess.check_output(["netsh", "wlan", "show", "profiles"], encoding="cp860")
+
+    sg.theme("Lightgray1")
+    menu = [
+        [sg.Multiline(dados, size=(95,35))],
+        [sg.Text('Digite o nome do wi-fi como esta a cima'), sg.Input(key='nome', size=(25,1))],
+        [sg.Button('Próximo')]
+    ]
+
+    win = sg.Window("Menu", menu, element_justification='c')
+    e, v = win.read()
+
+    wifi = v['nome'] 
+    info = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key", "=", "clear"], encoding="cp860")
+
+    if e == sg.WINDOW_CLOSED:
+        win.close()
+    
+    elif e == 'Próximo':
+        for line in info.split("\n"):
+            if "Conteúdo da Chave" in line:
+                posi = line.find(":")
+                senha = line[posi+2:]
+                sg.popup_ok(wifi + " = " + senha)
+                win.close()
+                main()
+main()    
